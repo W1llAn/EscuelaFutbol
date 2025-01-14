b934d4b0bd3d2e91b038c9b3df8b022d1f941d42
W1llAn <wchimborazo2840@uta.edu.ec>
2025-01-14 07:53:28 -0500

    Ingreso de las rutas para horarios

--- EscuelaFutbol/routes/web.php
+++ EscuelaFutbol/routes/web.php
@@ -6,5 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/Escuela/create',[EscuelaController::class,'create']);
-Route::get('/Escuela',[EscuelaController::class,'index']);
+Route::get('/Escuela/create', [EscuelaController::class, 'create']);
+Route::get('/Escuela', [EscuelaController::class, 'index']);
+Route::get('/Escuela/horarios', [EscuelaController::class, 'horarios']);
+Route::delete("/Escuela/horarios/{id} {horario}", [EscuelaController::class, 'destroy']);
+Route::put("/Escuela/horarios/{id} {horario} /edit", [EscuelaController::class, 'update']);
