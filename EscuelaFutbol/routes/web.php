cf704d7eed99fabaf6054cdd04f472b3a36ffb02
Mr-Dark65 <steevenloor2412@gmail.com>
2025-01-14 18:52:53 -0500

    Se implemento las rutas para las categorias

--- EscuelaFutbol/routes/web.php
+++ EscuelaFutbol/routes/web.php
@@ -11,3 +11,10 @@ Route::get('/Escuela', [EscuelaController::class, 'index']);
 Route::get('/Escuela/horarios', [EscuelaController::class, 'horarios']);
 Route::delete("/Escuela/horarios/{id} {horario}", [EscuelaController::class, 'destroy']);
 Route::put("/Escuela/horarios/{id} {horario} /edit", [EscuelaController::class, 'update']);
+
+//rutas para categorias
+Route::get('/Escuela/categorias', [EscuelaController::class, 'categorias'])->name('categorias.index');
+Route::get('/Escuela/categorias/create', [EscuelaController::class, 'crearCategoria'])->name('categorias.create');
+Route::get('/Escuela/categorias/{id}/edit', [EscuelaController::class, 'editarCategoria'])->name('categorias.edit');
+Route::put('/Escuela/categorias/{id}', [EscuelaController::class, 'updateCategoria'])->name('categorias.update');
+Route::delete('/Escuela/categorias/{id}', [EscuelaController::class, 'destroyCategoria'])->name('categorias.destroy');
